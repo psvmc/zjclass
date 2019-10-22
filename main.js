3df6d1d8fc36b3238822f7cacf6c677bf81ef0fc
psvmc <183518918@qq.com>
2019-10-22 14:53:51 +0800

    依赖

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require("electron");
 const electron = require("electron");
-const path = require("path");
 const {ipcMain} = require("electron");
 const {useCapture} = require("./views/capture/capture-main");
 
